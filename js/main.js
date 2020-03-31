cb3a0210bd8f64b19ff49598a65b9928315a22e0
Abdshammala1 <55294151+Abdshammala1@users.noreply.github.com>
2020-03-31 19:13:38 -0400

    added dev files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.abdal_btn').click(function(){
+        window.location.href = "abdal.html";
+    });
+
+    $('.xyz_btn').click(function(){
+        window.location.href = "xyz.html";
+    });
+});
