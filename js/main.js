3d7001e4e0402520db1ddbbe5830cbba6e609b98
Abdshammala1 <55294151+Abdshammala1@users.noreply.github.com>
2020-04-02 00:07:34 -0400

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.abdal_btn').click(function(){
-        window.location.href = "abdal.html";
-    });
-
-    $('.xyz_btn').click(function(){
-        window.location.href = "xyz.html";
-    });
-});
